aa361e7011d7fcd43777c8425fbc3df244941296
CastalJaemin <ehdrud1332@gmail.com>
2020-05-28 21:33:10 +0900

    updating Header(Header TItle)

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useLayoutEffect} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Like from '../screens/Like';
 import Movies from '../screens/Movies';
@@ -8,12 +8,25 @@ import Profile from '../screens/Profile';
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-    <Tabs.Navigator>
-        <Tabs.Screen name="Movies" component={Movies} />
-        <Tabs.Screen name="Search" component={Search} />
-        <Tabs.Screen name="Tv" component={Tv} />
-        <Tabs.Screen name="Like" component={Like} />
-        <Tabs.Screen name="Profile" component={Profile} />
-    </Tabs.Navigator>
-);
+const getHeaderName = route =>
+    route?.state?.routeNames[route.state.index] || "Movies";
+
+export default ({navigation, route}) => {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: getHeaderName(route)
+        });
+    }, [route]);
+    return (
+        <Tabs.Navigator>
+            <Tabs.Screen name="Movies" component={Movies}/>
+            <Tabs.Screen name="Search" component={Search}/>
+            <Tabs.Screen name="Tv" component={Tv}/>
+            <Tabs.Screen name="Like" component={Like}/>
+            <Tabs.Screen name="Profile" component={Profile}/>
+        </Tabs.Navigator>
+
+    )
+
+
+};
