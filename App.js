09790b9d0e9f8454f2e0ea2ab406f5c374a152a2
CastalJaemin <ehdrud1332@gmail.com>
2020-05-28 15:23:07 +0900

    Cache Fonts.

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
-import {Text, Image} from 'react-native';
+import {Text, Image, View} from 'react-native';
 import {AppLoading} from 'expo';
 import {Asset} from 'expo-asset';
+import *as Font from 'expo-font';
+import {Ionicons} from '@expo/vector-icons';
 
 const cacheImages = images =>
     images.map(image => {
@@ -12,6 +14,9 @@ const cacheImages = images =>
         }
     });
 
+const cacheFonts = fonts =>
+    fonts.map(font => [Font.loadAsync(font)]);
+
 export default function () {
 
     const [isReady, setIsReady] = useState(false);
@@ -20,6 +25,8 @@ export default function () {
             "https://images.unsplash.com/photo-1584486188544-dc2e1417aff1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
         ]);
         console.log(images);
+        const fonts = cacheFonts([Ionicons.font]);
+        return Promise.all([...images, ...fonts]);
     }
     const onFinish = () => setIsReady(true);
 
